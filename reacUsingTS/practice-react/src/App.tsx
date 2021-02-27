d9156c26e0284d4940a923c59a8a0ca106113aec
Ajay-the-programmer <56474908+Ajay-The-Programmer@users.noreply.github.com>
2021-02-27 11:36:55 +0530

    function component using props concept

--- reacUsingTS/practice-react/src/App.tsx
+++ reacUsingTS/practice-react/src/App.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "./App.css";
 
-const Coder = () => {
-  return <h1>Welcome to React Application</h1>;
+const Coder = (props: { name: string; status: string }) => {
+  return (
+    <div>
+      <h1>Welcome to React Application</h1>
+      <h2>{props.name}</h2>
+      <h3>{props.status}</h3>
+    </div>
+  );
 };
 
 class App extends React.Component {
   render() {
-    return (
+    return (  
       <div className="App">
-        <Coder />
-        <Coder />
-        <Coder />
+        <Coder name="Ajay" status="Programmer" />
       </div>
     );
   }
