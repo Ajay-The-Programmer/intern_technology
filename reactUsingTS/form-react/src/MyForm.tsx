2cfac6676510a7ba1f3179a0827acf5cfe5d6d5f
Ajay-the-programmer <56474908+Ajay-The-Programmer@users.noreply.github.com>
2021-03-03 13:10:45 +0530

    password digit validation

--- reactUsingTS/form-react/src/MyForm.tsx
+++ reactUsingTS/form-react/src/MyForm.tsx
@@ -2,16 +2,17 @@ import React from "react";
 class MyForm extends React.Component {
   state: { userName: string; password: null };
 
-  myChangeHandler = (event: { target: { value: any; name: any } }) => {
-   
+  myChangeHandler = (event: { target: { value: any; name: string } }) => {
     let nam = event.target.name;
-  
 
     let pass = event.target.value;
-  
+    if (nam === "password") {
+      if (!Number(pass)) {
+        alert("Please password Enter only as Degit ");
+      }
+    }
 
-    this.setState({ [nam]:pass});
-    
+    this.setState({ [nam]: pass });
   };
 
   constructor(props: any) {
@@ -28,7 +29,7 @@ class MyForm extends React.Component {
         <input type="text" name="userName" onChange={this.myChangeHandler} />
         <label>Enter Password</label>
         <input type="text" name="password" onChange={this.myChangeHandler} />
-        
+
         <h2>
           {this.state.userName} {this.state.password}
         </h2>
