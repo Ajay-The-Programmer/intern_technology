803559c9eecb6950f8806e5d3e8f4c811d94fe41
Ajay-the-programmer <56474908+Ajay-The-Programmer@users.noreply.github.com>
2021-03-03 13:03:39 +0530

    update

--- reactUsingTS/form-react/src/MyForm.tsx
+++ reactUsingTS/form-react/src/MyForm.tsx
@@ -1,32 +1,44 @@
 import React from "react";
 class MyForm extends React.Component {
-  state: { userName: string };
+  state: { userName: string; password: null };
 
-  myChangeHandler = (event: { target: { value: string } }) => {
-    this.setState({
-      userName: event.target.value,
-    });
-  };
-  onSubmitForm = (event: { preventDefault:()=>void}) => {
-    event.preventDefault();
-    alert("welcome you login successfull"+this.state.userName)
+  myChangeHandler = (event: { target: { value: any; name: any } }) => {
+   
+    let nam = event.target.name;
+  
+
+    let pass = event.target.value;
+  
+
+    this.setState({ [nam]:pass});
+    
   };
 
   constructor(props: any) {
     super(props);
     this.state = {
       userName: " ",
+      password: null,
     };
   }
   render() {
     return (
-      <form onSubmit={this.onSubmitForm}>
-        <label>Enter Your Name</label>
-        <input type="text" onChange={this.myChangeHandler} />
-        <input type="submit" />
-        <h2>{this.state.userName}</h2>
+      <form>
+        <label>Enter UserName</label>
+        <input type="text" name="userName" onChange={this.myChangeHandler} />
+        <label>Enter Password</label>
+        <input type="text" name="password" onChange={this.myChangeHandler} />
+        
+        <h2>
+          {this.state.userName} {this.state.password}
+        </h2>
       </form>
     );
   }
 }
 export default MyForm;
+
+//   onSubmitForm = (event: { preventDefault:()=>void}) => {
+//     event.preventDefault();
+//     alert("welcome you login successfull"+this.state.userName)
+//   };
